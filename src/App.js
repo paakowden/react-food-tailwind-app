4e28b1d2ededf6525775736b6f685a560f7e4043
paakowden <paakow1114@gmail.com>
2022-07-27 00:19:53 +0000

    Project Done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import Category from './components/Category';
+import Food from './components/Food';
+import HeadlineCards from './components/HeadlineCards';
+import Hero from './components/Hero';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div >
-     <h1>Hello</h1>
+     <Navbar />
+     <Hero />
+     <HeadlineCards />
+     <Food />
+     <Category />
     </div>
   );
 }
